8e3682e69bd407ef41efd3a0bafaf2052bc4ebe3
rakeshpraneel <59800595+rakeshpraneel@users.noreply.github.com>
2020-09-08 22:11:11 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,55 @@
+from flask import Flask, render_template, request
+from chatterbot import ChatBot
+from chatterbot.trainers import ChatterBotCorpusTrainer
+import time
+
+
+application = Flask(__name__)
+
+MyName = "Friday"
+chatbot = ChatBot(MyName, storage_adapter="chatterbot.storage.SQLStorageAdapter")
+trainer = ChatterBotCorpusTrainer(chatbot)
+#trainer.export_for_training('./export.yml')
+trainer.train("chatterbot.corpus.english.conversations")
+trainer.train("chatterbot.corpus.english.greetings")
+trainer.train("chatterbot.corpus.english")
+#trainer.export_for_training('./my_export.json')
+#userinput="What is your name"
+#if userinput == 'What is your name':
+#    output = "I m "+myname
+#else:
+#    output=str(chatbot.get_response(userinput))
+#print(output)
+
+
+@application.route("/")
+def home():
+    return render_template("index.html")
+
+
+@application.route("/get")
+def bot_inout():
+    subtext = request.args.get('msg')
+    message=str(subtext)
+    message=message.lower()
+    if message == 'what is your name' or message == 'what is your name?':
+        reply = "I'm Friday"
+        return reply
+    elif message == 'what is my name' or message == 'what is my name?':
+        reply = "I'm not an astrologer"
+        return reply
+    elif message.__contains__('bye') or message == 'see you':
+        reply = "It was good to spend time with you. Bye Bye!!!"
+        return reply
+    elif message.__contains__('fuck'):
+        reply = 'Dont speak bad words!!!\nI m really disappointed!!!'
+        return reply
+    elif message == "what's the time" or message == "what is the time":
+        Time = time.clock()
+        return Time
+    else:
+        return str(chatbot.get_response(subtext))
+
+
+if __name__ == '__main__':
+    application.run()
